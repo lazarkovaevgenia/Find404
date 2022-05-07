21de7c0abb3aab1a7a3a9a5e715ed813935e9dd3
lazarkovaevgenia <ealazarkova@gmail.com>
2022-05-08 03:11:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
@@ -47,4 +44,3 @@ def get_html(url):
 if __name__ == '__main__':
     find404(SITE, SITE)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
